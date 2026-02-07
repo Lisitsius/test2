e592a1867591c14968a4681459ae1368fc6366e1
Lisitsa Egor <eg.lisitsa@yandex.ru>
2026-02-07 14:21:47 +0300

    firstSave

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import './assets/main.css';  // Глобальные стили
+
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
